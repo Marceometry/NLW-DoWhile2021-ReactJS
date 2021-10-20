670d87ac5c00344cca229fd2743076b6081ec83c
Marceometry <marceometry@gmail.com>
2021-10-20 00:34:22 -0300

    finish base layout

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,63 @@
+import styles from './styles.module.scss'
+import logo from '../../assets/logo.svg'
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.wrapper}>
+      <img src={logo} alt='Logo' />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.content}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            quasi veniam maiores ipsam accusamus. Est, vero harum provident sed
+            illo sapiente dolorum debitis quidem iusto molestiae molestias quae.
+            Magnam, illo.
+          </p>
+
+          <div className={styles.user}>
+            <div className={styles.avatar}>
+              <img src="https://github.com/Marceometry.png" alt="Avatar" />
+            </div>
+
+            <span className={styles.username}>Marceometry</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.content}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            quasi veniam maiores ipsam accusamus. Est, vero harum provident sed
+            illo sapiente dolorum debitis quidem iusto molestiae molestias quae.
+            Magnam, illo.
+          </p>
+
+          <div className={styles.user}>
+            <div className={styles.avatar}>
+              <img src="https://github.com/Marceometry.png" alt="Avatar" />
+            </div>
+
+            <span className={styles.username}>Marceometry</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.content}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            quasi veniam maiores ipsam accusamus. Est, vero harum provident sed
+            illo sapiente dolorum debitis quidem iusto molestiae molestias quae.
+            Magnam, illo.
+          </p>
+
+          <div className={styles.user}>
+            <div className={styles.avatar}>
+              <img src="https://github.com/Marceometry.png" alt="Avatar" />
+            </div>
+
+            <span className={styles.username}>Marceometry</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
